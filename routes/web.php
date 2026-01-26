a8c1a0ae041c76385d3c34f71489071bd3757f0e
vinniee3807 <arlynncevill@gmail.com>
2026-01-26 09:23:56 +0700

    management link-unfinished.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdmLinksController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware(['auth', 'admin'])->group(function () {
-    // route admin
-});
+// Route::middleware(['auth', 'admin'])->group(function () {
+//     // route admin
+    
+// });
+
+Route::resource('/adm-links', AdmLinksController::class);
